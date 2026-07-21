9512b3416a84c3d8f42ade9bd464c924b1e9589c
Miguel Diogo <miguel.g.diogo@gmail.com>
2026-07-21 13:11:28 +0100

    chore: initialize RuneLite plugin project

---
+++ src/test/java/io/github/mgdiogo/huntertracker/ExamplePluginTest.java
@@ -0,0 +1,13 @@
+package io.github.mgdiogo.huntertracker;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class ExamplePluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(HunterTrackerPlugin.class);
+		RuneLite.main(args);
+	}
+}
